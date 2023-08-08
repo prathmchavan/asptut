869f50cf2a97d3f598e97377006c3cbef6c8eb1a
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2023-08-08 15:15:52 +0530

    learnt the hot reload and mvc structure

--- asp/Program.cs
+++ asp/Program.cs
@@ -1,21 +1,27 @@
+
+//we can manually pass argument 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// we write our dependency injection code after the builder initialization and before the build funtion
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+//http pipeline get the req from browser and checks the middelware and then finally that req gets to the our server code
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+//the below are some middleware i have used
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+//order of the middleware matters in the pipeline 
 app.UseRouting();
 
 app.UseAuthorization();
