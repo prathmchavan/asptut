aca08b49af400d2ccad1db0387f4963b3bdcc5ec
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2023-08-09 23:27:56 +0530

    connected to sql datbase and added the controller and new view for that controller

---
+++ asp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using asp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>option) : base(option) 
+        {
+            
+        }
+
+        public DbSet<Category>Categories { get; set; }
+    }
+}
