267e2e574888c145a50e40590ed74b1138e8c52f
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2023-08-09 23:31:25 +0530

    Revert "connected to sql datbase and added the controller and new view for that controller"
    This reverts commit aca08b49af400d2ccad1db0387f4963b3bdcc5ec.

--- asp/Program.cs
+++ asp/Program.cs
@@ -1,15 +1,7 @@
-
-using asp.Data;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("connect")
-    ));
 
 var app = builder.Build();
 
