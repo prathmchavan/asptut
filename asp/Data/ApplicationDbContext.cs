836fa1010dd7e03bc5a3cd638d1803ab2506c3e1
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2023-08-10 23:48:25 +0530

    created category controller and views for it

---
+++ asp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using asp.Models; // Add the correct using directive for the namespace where Category is defined
+
+namespace asp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
+
---
+++ asp/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace asp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateDateTime { get; set; }= DateTime.Now;
+    }
+}
